67b97846659ec1f46505fe67e3460e6e307df018
Shad Gregory <captshadg@gmail.com>
2012-12-28 16:35:14 -0600

    use rackunit

--- test.clj
+++ test.clj
@@ -1,24 +1,13 @@
 #lang reader "clojure.rkt"
-
-(println "hello\n")
-
-(eq? (= 1 1) true)
-
-(if true "yes")
-
-(println [1 2 3])
-(newline)
-
-(println "hello again\n")
-
-[x y]
-(pop [1 2 3]) ;[1 2]
-(pop '(1 2 3)) ;(2 3)
+(require rackunit)
 
 (defn factorial [n]
   (cond
    (<= n 1) 1
    :else (* n (factorial (dec n)))))
 
-(factorial 5) ;120
-                 
+(check-equal? #t (eq? (= 1 1) true))
+(check-equal? 120 (factorial 5))
+(check-equal? [1 2] (pop [1 2 3]))
+(check-equal? '(2 3) (pop '(1 2 3)))
+(check-equal? "yes" (if true "yes"))
